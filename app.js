bf0b82092f4d494ada043636629ac557d5ada31c
igor pirasoli <igorpirasoli@outlook.com>
2025-01-21 19:03:46 -0300

    deixando o jogo mais facil

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 50');
 }
 
 function verificarChute() {
@@ -40,9 +40,9 @@ function verificarChute() {
 
 function gerarNumeroAleatorio() {
     let quantidadeDeElementosNaLista = listaDeNumerosEscolhidos.length
-    let numeroEscolhido =  parseInt(Math.random() * 100 + 1);
+    let numeroEscolhido =  parseInt(Math.random() * 50 + 1);
 
-    if(quantidadeDeElementosNaLista == 100) {
+    if(quantidadeDeElementosNaLista == 50) {
         listaDeNumerosEscolhidos = [];
     }
 
